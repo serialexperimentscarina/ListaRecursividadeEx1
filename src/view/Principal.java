a7f943a7055d422cf6230297eab6f3cb74bc419f
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-04-25 22:00:51 -0300

    refactor: input retry

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -9,17 +9,21 @@ public class Principal {
 	public static void main(String[] args) {
 		SomatorioController somaCont = new SomatorioController();
 		
-		String input = JOptionPane.showInputDialog("Digite um número natural: ", "");
-		if (input == null) {
-			JOptionPane.showMessageDialog(null, "Encerrando");
-		} else {
+		do {
 			try {
+				String input = JOptionPane.showInputDialog("Digite um número natural: ", "");
+				if (input == null) {
+					JOptionPane.showMessageDialog(null, "Encerrando");
+					System.exit(0);
+				}
 				int n = Integer.parseInt(input);
 				JOptionPane.showMessageDialog(null, "Somatório dos " + n + " primeiros números naturais: " + somaCont.somatorio(n));
 			} catch (NumberFormatException e) {
 				JOptionPane.showMessageDialog(null, "Número inválido");
+				continue;
 			}
-		}
+			break;
+		} while(true);
 	}
 
 }
